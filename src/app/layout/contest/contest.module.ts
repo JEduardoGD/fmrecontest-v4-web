5267b233f524732f62940473047e0e24e7653802
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2020-01-12 14:35:27 -0600

    ading datatables

--- src/app/layout/contest/contest.module.ts
+++ src/app/layout/contest/contest.module.ts
@@ -6,12 +6,15 @@ import { ContestRoutingModule } from './contest-routing.module';
 import { ContestComponent } from './contest.component';
 import { PageHeaderModule } from './../../shared';
 
+import { DataTableModule } from 'ng-angular8-datatable';
+
 @NgModule({
   declarations: [ContestComponent],
   imports: [
     CommonModule,
     ContestRoutingModule,
-    PageHeaderModule
+    PageHeaderModule,
+    DataTableModule
   ]
 })
 export class ContestModule { }
