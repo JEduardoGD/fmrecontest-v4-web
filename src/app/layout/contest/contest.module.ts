5eff984cf2bf5bd207ad981f72ded962fcee7f28
Eduardo Gutierrez <jgutierrezd@syesoftware.com>
2020-01-17 14:16:59 -0600

    removing old datatable

--- src/app/layout/contest/contest.module.ts
+++ src/app/layout/contest/contest.module.ts
@@ -6,15 +6,12 @@ import { ContestRoutingModule } from './contest-routing.module';
 import { ContestComponent } from './contest.component';
 import { PageHeaderModule } from './../../shared';
 
-import { DataTableModule } from 'ng-angular8-datatable';
-
 @NgModule({
   declarations: [ContestComponent],
   imports: [
     CommonModule,
     ContestRoutingModule,
-    PageHeaderModule,
-    DataTableModule
+    PageHeaderModule
   ]
 })
 export class ContestModule { }
