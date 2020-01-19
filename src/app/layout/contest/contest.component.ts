7140b439b0211f99ea3ef98655f71d7ee96ea239
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2020-01-19 08:27:36 -0600

    adding error message

--- src/app/layout/contest/contest.component.ts
+++ src/app/layout/contest/contest.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { routerTransition } from 'src/app/router.animations';
 import { ContestService } from 'src/app/services/contest.service';
 import { ContestModel } from 'src/app/model/contest.model';
+import { SweetAlertService } from 'src/app/services/sweet.alert.service';
 
 @Component({
   selector: 'app-contest',
@@ -13,7 +14,9 @@ export class ContestComponent implements OnInit {
 
   listContest: Array<ContestModel>;
 
-  constructor(private contestService: ContestService) { }
+  constructor(
+    private contestService: ContestService,
+    private sweetAlertService: SweetAlertService) { }
 
   ngOnInit() {
     this.onLoadContests();
@@ -24,6 +27,10 @@ export class ContestComponent implements OnInit {
     .then(u => {
       const x: Array<ContestModel> = u.data;
       this.listContest = x;
+    })
+    .catch(e => {
+      this.sweetAlertService.errorMessage('Error', 'Intente mas tarde');
+      console.error(e);
     });
   }
 
