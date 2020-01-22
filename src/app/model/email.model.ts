10757747081ad181597e35b7619c5b8044742403
Eduardo Gutierrez <jgutierrezd@syesoftware.com>
2020-01-22 16:45:24 -0600

    adding ReceivedDate

--- src/app/model/email.model.ts
+++ src/app/model/email.model.ts
@@ -2,6 +2,7 @@ export class EmailModel {
     id: string;
     subject: string;
     sentDate: string;
+    setReceivedDate: string;
     recipientsFromName: string;
     recipientsFromAddress: string;
     emailStatus: string;
